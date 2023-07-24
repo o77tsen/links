424648cf8aa10da2f0cd040485ebdeb4c4ccdd00
o77tsen <astrowavefunction@gmail.com>
2023-07-24 17:49:29 +1000

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/main.scss";
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import SEOConfig from "../../next-seo.config";
+import { Analytics } from "@/scripts/analytics";
 import theme from "@/utils/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEOConfig}  />
+      <Analytics />
       <Component {...pageProps} />
       <style jsx global>
         {`
