027e80524f5bb0c1be05182dca6e431f60b9b892
o77tsen <astrowavefunction@gmail.com>
2023-07-24 13:09:10 +1000

    add base styling and content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@/styles/main.scss";
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import SEOConfig from "../../next-seo.config";
